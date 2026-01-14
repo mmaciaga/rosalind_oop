d2414822ad1c735158cee6bff9c13f5eb74d1e26
mmaciaga <magda.maciaga@gmail.com>
2026-01-14 10:15:21 +0100

    made it prettier

--- rosalind/main.cpp
+++ rosalind/main.cpp
@@ -9,14 +9,23 @@ using namespace std;
 
 int main() {
     solution s;
+    cout << "---- COUNTING DNA NUCLEOTIDES ----" << endl;
     s.c_nt();
+    cout << "---- TRANSCRIBING DNA INTO RNA ----" << endl;
     s.transcribe();
+    cout << "---- COMPLEMENTING A STRAND OF DNA ----" << endl;
     s.rev_comp();
+    cout << "---- TRANSLATING RNA INTO PROTEIN ----" << endl;
     s.translate();
+    cout << "---- INFERRING MRNA FROM PROTEIN ----" << endl;
     s.mrna();
-    s.fib();
-    s.dead_rabbits();
+    cout << "---- CALCULATING PROTEIN MASS ----" << endl;
     s.protein_mass();
+    cout << "---- RABBITS AND RECURRENCE RELATIONS ----" << endl;
+    s.fib();
+    cout << "---- MORTAL FIBONACCI RABBITS ----" << endl;
+    s.dead_rabbits();
+    cout << "---- COUNTING POINT MUTATIONS ----" << endl;
     s.hamm();
     return 0;
 }
